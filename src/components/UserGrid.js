7c2352e727e8bd070b9264e45a129c3d0cc059e2
Subkhon Amrulloev <amrulloev.suhon@gmail.com>
2022-02-13 18:55:47 +0900

    can remove users by checkig grid reference rows

--- src/components/UserGrid.js
+++ src/components/UserGrid.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { useState } from "react";
 
 const UserGrid = (props) => {
   const { users } = props;
+  const gridRef = useRef(null);
+
   const onCellChanged = async (params) => {
     console.log(
       "oldValue is " + params.oldValue + " , new value is " + params.newValue
@@ -25,7 +27,7 @@ const UserGrid = (props) => {
   };
 
   const [columnDefs] = useState([
-    { field: "id", sortable: true, filter: true },
+    { field: "id", sortable: true, filter: true, checkboxSelection: true },
     {
       field: "name",
       sortable: true,
@@ -42,11 +44,46 @@ const UserGrid = (props) => {
     },
   ]);
 
+  const showSelectedNodes = (e) => {
+    const selectedNodes = gridRef.current.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringRep = selectedData
+      .map((node) => `${node.name} ${node.email}`)
+      .join(", ");
+    console.log(selectedDataStringRep);
+    return selectedData;
+  };
+
+  const removeSelectedNodes = (e) => {
+    const selectedData = showSelectedNodes(e);
+    selectedData.map(async (node) => {
+      const response = await fetch("/api/users/remove", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: node.id,
+          name: node.name,
+          email: node.email,
+        }),
+      });
+      const response_json = await response.json();
+      console.log("response json after removing " + response_json["message"]);
+      props.setMessage(response_json["message"]);
+    });
+  };
+
   return (
     <div>
       <h2>Users</h2>
+      <button onClick={showSelectedNodes}>Selected nodes</button>
+      <button onClick={removeSelectedNodes}>Remove Selected nodes</button>
       <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-        <AgGridReact rowData={users} columnDefs={columnDefs}></AgGridReact>
+        <AgGridReact
+          ref={gridRef}
+          rowData={users}
+          columnDefs={columnDefs}
+          rowSelection="multiple"
+        ></AgGridReact>
       </div>
     </div>
   );
